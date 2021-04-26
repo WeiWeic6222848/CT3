3f588885f0f855eac64d8d066760519fc6486759
Wei Wei <Wei.Wei@student.uantwerpen.be>
2021-04-26 18:58:34 +0200

    initial commit with preprocess

---
+++ preprocess.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+seedMovies = pd.read_csv("./dataset/test-set.csv")
+
+pairJudgement = pd.read_csv("./dataset/pair-responses.csv")
+# print(pairJudgement)
+positiveJudgement = pairJudgement.groupby(by=["movieId", "neighborId"]) \
+    .filter(lambda x: len(x) >= 2) \
+    .groupby(by=["movieId", "neighborId"]) \
+    .mean()
+
+positiveJudgement = positiveJudgement[positiveJudgement.sim > 3].groupby(
+    by=["movieId"]).filter(lambda x: len(x) >= 4)
+print(positiveJudgement.groupby(
+    by=["movieId"]).count())
