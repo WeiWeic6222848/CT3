ced5c3e09500f267d3e0b6effbf3d43d5beb80d0
Wei Wei <Wei.Wei@student.uantwerpen.be>
2021-04-26 21:43:58 +0200

    preprocess movie ratings is identical

--- preprocess.py
+++ preprocess.py
@@ -13,3 +13,11 @@ positiveJudgement = positiveJudgement[positiveJudgement.sim >= 2].groupby(
     by=["movieId"]).filter(lambda x: len(x) > 4)
 
 print(positiveJudgement.groupby(by=["movieId"]).count().mean())
+
+allMovies = pd.read_csv("./dataset/movies.csv")
+allRatings = pd.read_csv("./dataset/ratings.csv")
+positiveRatings = allRatings[allRatings.rating >= 3]
+positiveRatings = positiveRatings[positiveRatings.groupby('movieId')['rating'].transform('count').ge(30)]
+
+
+print(positiveRatings)
